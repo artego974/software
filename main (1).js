c8279347c8d19fae1b95d33ec8ed57444363d10a
artego <162368460+artego974@users.noreply.github.com>
2024-03-25 17:05:46 -0300

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,31 @@
+var a = 9
+var b = 10
+var c =9
+var d = 5
+if((a>10 && a<20) || (b === 5)){
+print('true')
+} else {
+    print('false')
+}
+
+if ((a>10) || (b == 5)){
+    print ('true')
+}else{
+    print (' false')
+}
+if (a>10 && a < 20){
+    print('true')
+}else{
+    print ('false')
+}
+if ((a> 10)||(b===5) && (c===10)){
+    print('true')
+}else{
+    print (' false')
+}
+if (((a>10) ||(b===5))&& (c!=10)&& (d!=20)){
+    print("true")
+} else {
+    print('false')
+}
+
